af44098241bc0a869c28196784ba3731315fdbfe
MyPictures <b.afrim@gmail.com>
2013-05-23 20:04:29 +0200

    First commit

---
+++ src/net/nocheatplus/fakescetchjoin/FakeScetchJoin.java
@@ -0,0 +1,26 @@
+package net.nocheatplus.fakescetchjoin;
+ 
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public final class FakeScetchJoin extends JavaPlugin implements Listener {
+
+	@Override
+    public void onEnable(){
+		getServer().getPluginManager().registerEvents(this, this);
+    	getLogger().info("FakeScetchJoin enabled!");
+	}
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent evt) { 
+    	getServer().broadcastMessage(ChatColor.YELLOW + "Scetch joined the game");
+    }
+ 
+    @Override
+    public void onDisable() {
+    	getLogger().info("FakeScetchJoin disabled!");
+    }
+}
